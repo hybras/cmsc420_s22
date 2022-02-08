ec22a6b8b26834d3f5c912938696e713d0abd1a5
hybras <24651269+hybras@users.noreply.github.com>
2022-02-08 13:49:14 -0500

    Remove extra comments

--- project0/src/main/java/cmsc420_s22/EmptyListException.java
+++ project0/src/main/java/cmsc420_s22/EmptyListException.java
@@ -1,14 +1,5 @@
 package cmsc420_s22;
 
-/**
- * DualList (skeleton)
- * <p>
- * MODIFY THE FOLLOWING CLASS.
- * <p>
- * You are free to make whatever changes you like or to create additional
- * classes and files.
- */
-
 public final class EmptyListException extends Exception {
 
     public EmptyListException() {
