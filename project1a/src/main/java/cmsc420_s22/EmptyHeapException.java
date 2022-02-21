e40f428dcf2844a16435801a85635f5238b6d671
hybras <24651269+hybras@users.noreply.github.com>
2022-02-20 21:22:24 -0500

    Fix message for exception

--- project1a/src/main/java/cmsc420_s22/EmptyHeapException.java
+++ project1a/src/main/java/cmsc420_s22/EmptyHeapException.java
@@ -3,6 +3,6 @@ package cmsc420_s22;
 public final class EmptyHeapException extends Exception {
 
     public EmptyHeapException() {
-        super("Attempt to extract from an empty list");
+        super("Empty heap");
     }
 }
