3582086a89b26d62726093c8845666c46e8c121a
hybras <24651269+hybras@users.noreply.github.com>
2022-03-21 22:16:39 -0400

    Shorten project folder names

---
+++ p1a/src/main/java/cmsc420_s22/QuakeHeap.java
@@ -0,0 +1,17 @@
+package cmsc420_s22;
+
+import java.util.ArrayList;
+
+public class QuakeHeap<Key extends Comparable<Key>, Value> {
+
+	class Node { /* a node in your tree */ }
+	public class Locator { /* locates a node */ }
+
+	public QuakeHeap(int nLevels) { /* ... */ }
+	public void clear() { /* ... */ }
+	public Locator insert(Key x, Value v) { /* ... */ return null; }
+	public Key getMinKey() throws EmptyHeapException { /* ... */ return null; }
+	public int getMaxLevel(Locator r) { /* ... */ return 0; }
+	public ArrayList<String> listHeap() { /* ... */ return new ArrayList<String>(); }
+
+}
